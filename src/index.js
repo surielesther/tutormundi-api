c2d5a05e7e0ba7bc8a03f6e14aa1e171bd1a7035
surielesther <esther.suriel@hotmail.com>
2023-02-13 22:30:06 -0300

    autorização cors corretamente configurada

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import express from "express";
 
 const app = express();
+const cors = require("cors");
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET");
+  app.use(cors);
+  next();
+});
 
 app.get("/feedback/:userReview", (req, res) => {
   const { userReview } = req.params;
